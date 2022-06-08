05f031df8872d4f9b98102d46a22add90e0c419b
Valdesrep <90558354+Valdesrep@users.noreply.github.com>
2022-06-08 13:30:12 +0300

    finish localStorage

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -44,7 +44,18 @@ export class App extends Component {
   onChangeContactFilter = e => {
     this.setState({ filter: e.target.value });
   };
-
+  componentDidMount() {
+    const contacts = localStorage.getItem('contacts');
+    const parsedContacts = JSON.parse(contacts);
+    if (parsedContacts) {
+      this.setState({ contacts: parsedContacts });
+    }
+  }
+  componentDidUpdate(prevProps, prevState) {
+    if (this.state.contacts !== prevState.contacts) {
+      localStorage.setItem('contacts', JSON.stringify(this.state.contacts));
+    }
+  }
   render() {
     const filterContacts = this.getFilterContacts();
     return (
